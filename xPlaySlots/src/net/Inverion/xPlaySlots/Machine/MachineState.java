49a8312c93a47578b7d1584436b9ddb1a2344e20
Simon Erlandsson <simonsigge@hotmail.com>
2021-07-21 22:11:12 +0200

    Cleanup

--- xPlaySlots/src/net/Inverion/xPlaySlots/Machine/MachineState.java
+++
@@ -1,5 +0,0 @@
-package net.Inverion.xPlaySlots.Machine;
-
-public enum MachineState {
-	WAITING, PRE_ROLL, ROLLING, POST_ROLL, END_ROLL;
-}
--- xPlaySlots/src/net/Inverion/xPlaySlots/Machine/TickType.java
+++
@@ -1,5 +0,0 @@
-package net.Inverion.xPlaySlots.Machine;
-
-public enum TickType {
-	TWENTY_TICKS, TEN_TICKS;
-}
